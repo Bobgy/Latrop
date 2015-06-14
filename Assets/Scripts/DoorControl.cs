24b75dcff39e7b2c6d09c7d2570f5f0fc93f1712
Bobgy <gongyuan94@gmail.com>
2015-06-14 20:01:41 +0800

    Added ControlledDoor

---
+++ Assets/Scripts/DoorControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorControl : MonoBehaviour {
+
+    public Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+        targetPosition = GetComponent<Transform>().position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform tf = GetComponent<Transform>();
+        tf.position = (tf.position * 99.0f + targetPosition) / 100.0f;
+	}
+}
--- Assets/Scripts/TouchDetection.cs
+++ Assets/Scripts/TouchDetection.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class TouchDetection : MonoBehaviour {
 
+    public GameObject door;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -16,9 +18,21 @@ public class TouchDetection : MonoBehaviour {
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("Trigger On");
+        DoorControl dc = door.GetComponent<DoorControl>();
+        dc.targetPosition += Vector3.up * 3.0f;
+        /*
+        Transform tf = door.GetComponent<Transform>();
+        tf.position += Vector3.up * 2.0f;
+        */
     }
 
     void OnTriggerExit(Collider other) {
         Debug.Log("Trigger Off");
+        DoorControl dc = door.GetComponent<DoorControl>();
+        dc.targetPosition += Vector3.down * 3.0f;
+        /*
+        Transform tf = door.GetComponent<Transform>();
+        tf.position += Vector3.down * 2.0f;
+        */
     }
 }
