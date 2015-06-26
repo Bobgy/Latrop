d6ae151b41b8a9ca76416c59e006440be65edbc9
zimpha <zimpha@gmail.com>
2015-06-26 23:32:23 +0800

    add menu scripts on levels

---
+++ Assets/Scripts/LevelMenuScript.cs
@@ -0,0 +1,29 @@
+﻿/*
+在对应的scene里面设置好currentLevel和nextLevel
+然后把脚本添加到player上
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class LevelMenuScript : MonoBehaviour {
+	public string currentLevel;
+	public string nextLevel;
+
+	void OnGUI() {
+		const int buttonWidth = 84;
+		const int buttonHeight = 30;
+		Rect RetryButton = new Rect(Screen.width - 270, 10, buttonWidth, buttonHeight);
+		Rect NextButton = new Rect(Screen.width - 180, 10, buttonWidth, buttonHeight);
+		Rect ReturnButton = new Rect(Screen.width - 90, 10, buttonWidth, buttonHeight);
+		if (currentLevel != "" && GUI.Button(RetryButton, "RETRY")) {
+			Application.LoadLevel(currentLevel);
+		}
+		if (nextLevel != "" && GUI.Button(NextButton, "NEXT")) {
+			Application.LoadLevel(nextLevel);
+		}
+		if (GUI.Button(ReturnButton, "BACK")) {
+			Application.LoadLevel("MainMenu");
+		}
+	}
+}
