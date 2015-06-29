21ee0b0c5a2860b64b490c6948aa5436af70f5ed
fjysmall <fengjingyi1122@gmail.com>
2015-06-30 03:11:03 +0800

    Fixed Catch

--- Assets/Scripts/Catch.cs
+++ Assets/Scripts/Catch.cs
@@ -6,15 +6,14 @@ using System.Collections;
 //drag = 5
 //Angular Dray = 5
 public class Catch : MonoBehaviour {
-	public float Xspeed = 1.0f;
-	public float Yspeed = 1.0f;
-	public float Fspeed = 1.0f;
 	public float CatchDistance = 2.0f;
 
 	GameObject obj = null;
 	bool flag = false;
-	Vector3 lastPos;
 	Vector3 centerPos = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+
+	float myDistance;
+
 	void Start () {
 		Cursor.visible = false;
 		Cursor.lockState = CursorLockMode.Locked;
@@ -37,7 +36,7 @@ public class Catch : MonoBehaviour {
 				if (Physics.Raycast(ray, out hit, CatchDistance)) {
 					hitObj = hit.collider.gameObject;
 					obj = hitObj.transform.gameObject;
-					lastPos = this.transform.position;
+					myDistance = Vector3.Distance(this.transform.position, obj.transform.position);
 					if (obj != null && IsCatchable(obj)) {
 						Rigidbody rb = obj.gameObject.GetComponent<Rigidbody>();
 						rb.useGravity = false;
@@ -54,25 +53,16 @@ public class Catch : MonoBehaviour {
 		}
 		//move the the cursor
 		if (flag == true) {
-			Vector3 NextPos = obj.transform.position;
-			Vector3 offset = this.transform.position - lastPos;
-			offset.y = 0.0f;
-			NextPos += offset * Fspeed;
-			float dy = Input.GetAxis("Mouse Y") * Yspeed;
-			float dx = Input.GetAxis("Mouse X") * Xspeed;
-			if (dy > 0.2f) {
-				dy = 0.2f;
-			}
-			if (dy < -0.2f) {
-				dy = -0.2f;
-			}
-			NextPos += new Vector3(0, dy, 0);
-			obj.transform.position = NextPos;
-			//!
-			obj.transform.LookAt(this.transform.position);
-			obj.transform.Translate(new Vector3(-dx, 0, 0));
-			//!
-			lastPos = this.transform.position;
+			Ray ray = Camera.main.ScreenPointToRay(centerPos);
+
+			float k = (
+				ray.direction.x * ray.direction.x + 
+				ray.direction.y * ray.direction.y + 
+				ray.direction.z * ray.direction.z
+				)	/
+				myDistance * myDistance;
+			Vector3 JumpPos = ray.origin + ray.direction * k;
+			obj.transform.position = JumpPos;
 		}
 	}
 
