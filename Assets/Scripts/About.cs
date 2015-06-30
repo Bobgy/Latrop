4db99c4fb0f8cd464751c1def60e549a2945fb01
zhujiale2 <3120000346@zju.edu.cn>
2015-07-01 00:18:23 +0800

    add ending

---
+++ Assets/Scripts/About.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class About : MonoBehaviour {
+
+    void Start()
+    {
+		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.None;
+    }
+
+	void OnGUI() {
+		const int buttonWidth = 84;
+		const int buttonHeight = 30;
+		Rect AboutButton = new Rect(30, Screen.height - 40, buttonWidth, buttonHeight);
+		if (GUI.Button(AboutButton, "BACK")) {
+			Application.LoadLevel("MainMenu");
+		}
+	}
+}
---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Win : MonoBehaviour {
+
+    public GameObject light;
+    public GameObject camera;
+    public float step = 0.005f;
+	 
+    bool win = false;
+	// Use this for initialization
+	void Start () {
+		RenderSettings.ambientIntensity = 1;
+		light.GetComponent<Light>().intensity = 1;
+        //win = true;
+	}
+
+	void OnTriggerEnter(Collider other) {
+        Debug.Log("hit win");
+        if (other.gameObject.tag == "Player")
+        {
+            RenderSettings.skybox = null;
+            camera.GetComponent<Camera>().backgroundColor = Color.black;
+            win = true;
+            Debug.Log("player hit");
+        }
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(win)
+        {
+            RenderSettings.ambientIntensity -= step;
+            light.GetComponent<Light>().intensity -= step;
+            if (RenderSettings.ambientIntensity < 0.01f)
+                Application.LoadLevel("about");
+        }
+	}
+}
